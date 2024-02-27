e088a314f64056a5b21d7e39db33e89501aacd51
centurion <centurionoff.dev@proton.me>
2024-02-26 23:04:10 -0300

    test: implemented tests for Trade.cancel_order/3 function

--- test/bingx/api/trade_test.exs
+++ test/bingx/api/trade_test.exs
@@ -11,7 +11,8 @@ defmodule BingX.API.TradeTest do
   use Patch
 
   alias BingX.API.{QueryParams, Headers, Trade, Exception}
-  alias BingX.API.Trade.{PlaceOrderResponse, CancelAllOrdersResponse}
+  alias BingX.API.Trade.{PlaceOrderResponse, CancelAllOrdersResponse, CancelOrderResponse}
+  alias BingX.Order
   alias BingX.Order.TriggerMarket
 
   @origin Application.compile_env!(:bingx, :origin)
@@ -187,8 +188,7 @@ defmodule BingX.API.TradeTest do
 
       patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
 
-      params = %{symbol: "BTC-USDT"}
-      Trade.cancel_all_orders(params, api_key, secret_key)
+      Trade.cancel_all_orders("BTC-USDT", api_key, secret_key)
 
       assert_called_once(HTTPoison.delete(_endpoint, _headers, _options))
     end
@@ -198,8 +198,7 @@ defmodule BingX.API.TradeTest do
 
       patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
 
-      params = %{symbol: "BTC-USDT"}
-      Trade.cancel_all_orders(params, api_key, secret_key)
+      Trade.cancel_all_orders("BTC-USDT", api_key, secret_key)
 
       assert_called_once(HTTPoison.delete(^endpoint, _headers, _options))
     end
@@ -212,8 +211,7 @@ defmodule BingX.API.TradeTest do
       patch(Headers, :append_api_key, headers)
       patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
 
-      params = %{symbol: "BTC-USDT"}
-      Trade.cancel_all_orders(params, api_key, secret_key)
+      Trade.cancel_all_orders("BTC-USDT", api_key, secret_key)
 
       assert_called_once(Headers.append_api_key(_, ^api_key))
       assert_called_once(HTTPoison.delete(_url, ^headers, _options))
@@ -226,20 +224,9 @@ defmodule BingX.API.TradeTest do
 
       patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
 
-      params = %{symbol: "BTC-USDT"}
-      Trade.cancel_all_orders(params, api_key, secret_key)
+      Trade.cancel_all_orders("BTC-USDT", api_key, secret_key)
 
       assert_called_once(HTTPoison.delete(_url, _headers, params: %{"symbol" => ^symbol}))
-    end
-
-    test "should raise error for missed symbol param", context do
-      %{api_key: api_key, secret_key: secret_key} = context
-
-      patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
-
-      assert_raise ArgumentError, 
-        "expected :symbol param to be given", 
-        fn -> Trade.cancel_all_orders(%{}, api_key, secret_key) end
     end
 
     test "should request with receive window in query params", context do
@@ -255,8 +242,7 @@ defmodule BingX.API.TradeTest do
 
       patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
 
-      params = %{symbol: "BTC-USDT"}
-      Trade.cancel_all_orders(params, api_key, secret_key)
+      Trade.cancel_all_orders("BTC-USDT", api_key, secret_key)
 
       assert_called_once(HTTPoison.delete(_url, _headers, params: %{"RECEIVE_WINDOW" => ^receive_window}))
 
@@ -271,8 +257,7 @@ defmodule BingX.API.TradeTest do
 
       patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
 
-      params = %{symbol: "BTC-USDT"}
-      Trade.cancel_all_orders(params, api_key, secret_key)
+      Trade.cancel_all_orders("BTC-USDT", api_key, secret_key)
 
       assert_called_once(HTTPoison.delete(_url, _headers, params: %{"TIMESTAMP" => ^timestamp}))
       assert_called_once(QueryParams.append_timestamp(_params))
@@ -289,8 +274,7 @@ defmodule BingX.API.TradeTest do
 
       patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
 
-      params = %{symbol: "BTC-USDT"}
-      Trade.cancel_all_orders(params, api_key, secret_key)
+      Trade.cancel_all_orders("BTC-USDT", api_key, secret_key)
 
       assert_called_once(HTTPoison.delete(_url, _headers, params: %{"SIGNATURE" => ^signature}))
       assert_called_once(QueryParams.append_signature(_params, ^secret_key))
@@ -303,8 +287,7 @@ defmodule BingX.API.TradeTest do
 
       patch(HTTPoison, :delete, result)
 
-      params = %{symbol: "BTC-USDT"}
-      Trade.cancel_all_orders(params, api_key, secret_key)
+      Trade.cancel_all_orders("BTC-USDT", api_key, secret_key)
     end
 
     test "should return the original error if request is not 200", context do
@@ -314,8 +297,7 @@ defmodule BingX.API.TradeTest do
 
       patch(HTTPoison, :delete, result)
 
-      params = %{symbol: "BTC-USDT"}
-      assert ^result = Trade.cancel_all_orders(params, api_key, secret_key)
+      assert ^result = Trade.cancel_all_orders("BTC-USDT", api_key, secret_key)
     end
 
     test "should wrap the successful response in CancelAllOrdersResponse", context do
@@ -328,8 +310,7 @@ defmodule BingX.API.TradeTest do
       patch(HTTPoison, :delete, {:ok, %HTTPoison.Response{body: body, status_code: 200}})
       patch(CancelAllOrdersResponse, :new, struct)
 
-      params = %{symbol: "BTC-USDT"}
-      assert {:ok, ^struct} = Trade.cancel_all_orders(params, api_key, secret_key)
+      assert {:ok, ^struct} = Trade.cancel_all_orders("BTC-USDT", api_key, secret_key)
 
       assert_called_once(CancelAllOrdersResponse.new(^data))
     end
@@ -345,8 +326,215 @@ defmodule BingX.API.TradeTest do
       patch(HTTPoison, :delete, {:ok, %HTTPoison.Response{body: body, status_code: 200}})
       patch(Exception, :new, struct)
 
-      params = %{symbol: "BTC-USDT"}
-      assert {:error, ^struct} = Trade.cancel_all_orders(params, api_key, secret_key)
+      assert {:error, ^struct} = Trade.cancel_all_orders("BTC-USDT", api_key, secret_key)
+
+      assert_called_once(Exception.new(^code, ^message))
+    end
+  end
+
+  describe "BingX.API.Trade cancel_order/3" do
+    setup _context do
+      {:ok, endpoint: @origin <> "/openApi/swap/v2/trade/order"}
+    end
+
+    test "should make DELETE request", context do
+      %{api_key: api_key, secret_key: secret_key} = context
+
+      patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
+
+      order = %Order{symbol: "BTC-USDT"}
+      Trade.cancel_order(order, api_key, secret_key)
+
+      assert_called_once(HTTPoison.delete(_endpoint, _headers, _options))
+    end
+
+    test "should request correct endpoint", context do
+      %{api_key: api_key, secret_key: secret_key, endpoint: endpoint} = context
+
+      patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
+
+      order = %Order{symbol: "BTC-USDT"}
+      Trade.cancel_order(order, api_key, secret_key)
+
+      assert_called_once(HTTPoison.delete(^endpoint, _headers, _options))
+    end
+
+    test "should request with authentication in headers", context do
+      %{api_key: api_key, secret_key: secret_key} = context
+
+      headers = %{"API_KEY" => api_key}
+
+      patch(Headers, :append_api_key, headers)
+      patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
+
+      order = %Order{symbol: "BTC-USDT"}
+      Trade.cancel_order(order, api_key, secret_key)
+
+      assert_called_once(Headers.append_api_key(_, ^api_key))
+      assert_called_once(HTTPoison.delete(_url, ^headers, _options))
+    end
+
+    test "should request with symbol in query params", context do
+      %{api_key: api_key, secret_key: secret_key} = context
+
+      patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
+
+      symbol = "BTC-USDT"
+      order = %Order{symbol: symbol}
+      Trade.cancel_order(order, api_key, secret_key)
+
+      assert_called_once(HTTPoison.delete(_url, _headers, params: %{"symbol" => ^symbol}))
+    end
+
+    test "should raise error for missed symbol param", context do
+      %{api_key: api_key, secret_key: secret_key} = context
+
+      patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
+
+      assert_raise ArgumentError,
+                   "expected :symbol param to be given",
+                   fn -> Trade.cancel_order(%Order{}, api_key, secret_key) end
+    end
+
+    test "should request with default order id in query params", context do
+      %{api_key: api_key, secret_key: secret_key} = context
+
+      patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
+
+      order = %Order{symbol: "BTC-USDT"}
+      Trade.cancel_order(order, api_key, secret_key)
+
+      assert_called_once(HTTPoison.delete(_url, _headers, params: %{"orderId" => ""}))
+    end
+
+    test "should request with default client order id in query params", context do
+      %{api_key: api_key, secret_key: secret_key} = context
+
+      patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
+
+      order = %Order{symbol: "BTC-USDT"}
+      Trade.cancel_order(order, api_key, secret_key)
+
+      assert_called_once(HTTPoison.delete(_url, _headers, params: %{"clientOrderID" => ""}))
+    end
+
+    test "should request with the provided order id in query params", context do
+      %{api_key: api_key, secret_key: secret_key} = context
+
+      patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
+
+      order_id = "ORDER_ID_FOR_TEST"
+      order = %Order{symbol: "BTC-USDT", order_id: order_id}
+      Trade.cancel_order(order, api_key, secret_key)
+
+      assert_called_once(HTTPoison.delete(_url, _headers, params: %{"orderId" => ^order_id}))
+    end
+
+    test "should request with the provided client order id in query params", context do
+      %{api_key: api_key, secret_key: secret_key} = context
+
+      patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
+
+      client_order_id = "CLIENT_ORDER_ID_FOR_TEST"
+      order = %Order{symbol: "BTC-USDT", client_order_id: client_order_id}
+      Trade.cancel_order(order, api_key, secret_key)
+
+      assert_called_once(HTTPoison.delete(_url, _headers, params: %{"clientOrderID" => ^client_order_id}))
+    end
+
+    test "should request with receive window in query params", context do
+      %{api_key: api_key, secret_key: secret_key} = context
+
+      receive_window = 2_312_321_321
+
+      patch(
+        QueryParams,
+        :append_receive_window,
+        &Map.merge(&1, %{"RECEIVE_WINDOW" => receive_window})
+      )
+
+      patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
+
+      order = %Order{symbol: "BTC-USDT"}
+      Trade.cancel_order(order, api_key, secret_key)
+
+      assert_called_once(HTTPoison.delete(_url, _headers, params: %{"RECEIVE_WINDOW" => ^receive_window}))
+      assert_called_once(QueryParams.append_receive_window(_))
+    end
+
+    test "should request with timestamp in query params", context do
+      %{api_key: api_key, secret_key: secret_key} = context
+
+      timestamp = 102_312_321
+      patch(QueryParams, :append_timestamp, &Map.merge(&1, %{"TIMESTAMP" => timestamp}))
+
+      patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
+
+      order = %Order{symbol: "BTC-USDT"}
+      Trade.cancel_order(order, api_key, secret_key)
+
+      assert_called_once(HTTPoison.delete(_url, _headers, params: %{"TIMESTAMP" => ^timestamp}))
+      assert_called_once(QueryParams.append_timestamp(_params))
+    end
+
+    test "should request with signature in query params", context do
+      %{api_key: api_key, secret_key: secret_key} = context
+
+      signature = 102_312_321
+
+      patch(QueryParams, :append_signature, fn params, _secret_key ->
+        Map.merge(params, %{"SIGNATURE" => signature})
+      end)
+
+      patch(HTTPoison, :delete, {:error, %HTTPoison.Error{reason: :timeout}})
+
+      order = %Order{symbol: "BTC-USDT"}
+      Trade.cancel_order(order, api_key, secret_key)
+
+      assert_called_once(HTTPoison.delete(_url, _headers, params: %{"SIGNATURE" => ^signature}))
+      assert_called_once(QueryParams.append_signature(_params, ^secret_key))
+    end
+
+    test "should return the original error if request is not 200", context do
+      %{api_key: api_key, secret_key: secret_key} = context
+
+      result = {:error, %HTTPoison.Response{status_code: 301}}
+
+      patch(HTTPoison, :delete, result)
+
+      order = %Order{symbol: "BTC-USDT"}
+      assert ^result = Trade.cancel_order(order, api_key, secret_key)
+    end
+
+    test "should wrap the successful response in CancelAllOrdersResponse", context do
+      %{api_key: api_key, secret_key: secret_key} = context
+
+      data = %{"success" => "ALWAYS!"}
+      body = Jason.encode!(%{"code" => 0, "data" => %{"order" => data}})
+      struct = %{success: "ALWAYS!"}
+
+      patch(HTTPoison, :delete, {:ok, %HTTPoison.Response{body: body, status_code: 200}})
+      patch(CancelOrderResponse, :new, struct)
+
+      order = %Order{symbol: "BTC-USDT"}
+      assert {:ok, ^struct} = Trade.cancel_order(order, api_key, secret_key)
+
+      assert_called_once(CancelOrderResponse.new(^data))
+    end
+
+    test "should wrap the unsuccessful response in Exception", context do
+      %{api_key: api_key, secret_key: secret_key} = context
+
+      code = 1001
+      message = "Too funny"
+      body = Jason.encode!(%{"code" => code, "msg" => message})
+      struct = %{success: "NEVER!"}
+
+      patch(HTTPoison, :delete, {:ok, %HTTPoison.Response{body: body, status_code: 200}})
+      patch(Exception, :new, struct)
+
+      order = %Order{symbol: "BTC-USDT"}
+      assert {:error, ^struct} = Trade.cancel_order(order, api_key, secret_key)
 
       assert_called_once(Exception.new(^code, ^message))
     end
