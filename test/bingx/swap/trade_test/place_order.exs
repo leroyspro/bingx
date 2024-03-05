397ad23f1e4c94a58943b5b7afcfb09b4dc88e7f
centurion <centurionoff.dev@proton.me>
2024-03-05 19:43:03 -0300

    test: implemented tests for Swap.Trade.place_order/3

---
+++ test/bingx/swap/trade_test/place_order.exs
@@ -0,0 +1,142 @@
+defmodule BingX.Swap.TradeTest.PlaceOrder do
+  @moduledoc """
+  Module to test BingX.API.Trade.
+
+  ## ATTENTION
+  Patch **every** network request to the real world!
+  These tests are used only to verify internal logic of the module, nothing else.
+  """
+
+  use ExUnit.Case
+  use Patch
+
+  alias BingX.Exception
+  alias BingX.Swap.{PlaceOrderResponse, Trade, Order}
+  alias BingX.HTTP.{Client, Response, Error}
+  alias BingX.Swap.Trade.Contract
+
+  setup_all do
+    order =
+      %Order{
+        side: :buy,
+        position_side: :long,
+        price: 51000,
+        stop_price: 51000,
+        quantity: 0.0001,
+        symbol: "BTC-USDT"
+      }
+
+    {
+      :ok,
+      order: order,
+      api_key: "API_KEY_FOR_TEST", 
+      secret_key: "SECRET_KEY_FOR_TEST",
+      path: "/openApi/swap/v2/trade/order"
+    }
+  end
+
+  describe "BingX.API.Trade place_order/3" do
+    test "should make POST request", context do
+      %{api_key: api_key, secret_key: secret_key, order: order} = context
+
+      patch(Client, :signed_request, {:error, :http_error, %Error{message: :timeout}})
+
+      Trade.place_order(order, api_key, secret_key)
+
+      assert_called_once(Client.signed_request(:post, _path, _api_key, _secret_key, _options))
+    end
+
+    test "should request correct endpoint", context do
+      %{api_key: api_key, secret_key: secret_key, path: path, order: order} = context
+
+      patch(Client, :signed_request, {:error, :http_error, %Error{message: :timeout}})
+
+      Trade.place_order(order, api_key, secret_key)
+
+      assert_called_once(Client.signed_request(_method, ^path, _api_key, _secret_key, _options))
+    end
+
+    test "should make secure request using provided credentials", context do
+      %{api_key: api_key, secret_key: secret_key, order: order} = context
+
+      patch(Client, :signed_request, {:error, :http_error, %Error{message: :timeout}})
+
+      Trade.place_order(order, api_key, secret_key)
+
+      assert_called_once(Client.signed_request(_method, _path, ^api_key, ^secret_key, _options))
+    end
+
+    test "should put transformed Order struct into query params", context do
+      %{api_key: api_key, secret_key: secret_key, order: order} = context
+
+      contract = %{"K" => "V"}
+      options = [params: contract]
+
+      patch(Contract, :from_order, contract)
+      patch(Client, :signed_request, {:error, :http_error, %Error{message: :timeout}})
+
+      Trade.place_order(order, api_key, secret_key)
+
+      assert_called_once(Contract.from_order(^order))
+      assert_called_once(Client.signed_request(_method, _path, _api_key, _secret_key, ^options))
+    end
+
+    test "should return the original request http-error", context do
+      %{api_key: api_key, secret_key: secret_key, order: order} = context
+
+      error = {:error, :http_error, %Error{message: :timeout}}
+
+      patch(Contract, :from_order, %{"K" => "V"})
+      patch(Client, :signed_request, error)
+
+      assert ^error = Trade.place_order(order, api_key, secret_key)
+    end
+
+    test "should extract and validate response content", context do
+      %{api_key: api_key, secret_key: secret_key, order: order} = context
+
+      content = "X"
+      response = %Response{body: %{"data" => content}}
+      struct = %{"A" => :s}
+
+      patch(PlaceOrderResponse, :new, struct)
+      patch(Response, :get_response_payload, {:ok, content})
+      patch(Client, :signed_request, {:ok, response})
+
+      Trade.place_order(order, api_key, secret_key)
+
+      assert_called_once(Response.get_response_payload(^response))
+    end
+
+    test "should wrap the content into PlaceOrderResponse struct", context do
+      %{api_key: api_key, secret_key: secret_key, order: order} = context
+
+      content = "X"
+      response = %Response{body: %{"data" => content}}
+      struct = %{"A" => :s}
+
+      patch(PlaceOrderResponse, :new, struct)
+      patch(Response, :get_response_payload, {:ok, content})
+      patch(Client, :signed_request, {:ok, response})
+
+      assert {:ok, ^struct} = Trade.place_order(order, api_key, secret_key)
+
+      assert_called_once(PlaceOrderResponse.new(^content))
+      assert_called_once(Response.get_response_payload(^response))
+    end
+
+    test "should return the original content-error", context do
+      %{api_key: api_key, secret_key: secret_key, order: order} = context
+
+      error = {:error, :content_error, "yep"}
+      response = %Response{body: %{"data" => "a"}}
+
+      patch(Response, :get_response_payload, error)
+      patch(Client, :signed_request, {:ok, response})
+
+      assert ^error = Trade.place_order(order, api_key, secret_key)
+
+      assert_called_once(Response.get_response_payload(^response))
+    end
+  end
+end
