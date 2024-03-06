e92936eebfce47c487dd614b054f322b43899bbe
centurion <centurionoff.dev@proton.me>
2024-03-06 01:56:26 -0300

    test: implemented basic tests for Swap.CancelAllOrdersResponse module

---
+++ test/bingx/swap/responses/cancel_all_orders_response.exs
@@ -0,0 +1,34 @@
+defmodule BingX.Swap.CancelAllOrdersResponseTest do
+  use ExUnit.Case
+  use Patch
+
+  alias BingX.Swap.CancelAllOrdersResponse
+  alias BingX.Swap.Trade.CanceledOrder
+
+  describe "BingX.Swap.CancelAllOrdersResponse new/1" do
+    test "should return empty struct without params" do
+      assert %CancelAllOrdersResponse{} = CancelAllOrdersResponse.new(%{})
+    end
+  end
+
+  describe "BingX.Swap.CancelAllOrdersResponse new/1 (transforming)" do
+    test "should be tolerant to wrong interface" do
+      assert %CancelAllOrdersResponse{} = CancelAllOrdersResponse.new(%{"x" => "x"})
+    end
+
+    test "should retrieve and transform succeeded orders into CanceledOrder struct" do
+      order_a = %{a: "a"} 
+      order_b = %{b: "b"}
+      orders = [order_a, order_b]
+
+      patch(CanceledOrder, :new, %{c: "c"})
+
+      assert %CancelAllOrdersResponse{
+        succeeded: [%{c: "c"}, %{c: "c"}]
+      } = CancelAllOrdersResponse.new(%{"success" => orders})
+
+      assert_called_once(CanceledOrder.new(^order_a))
+      assert_called_once(CanceledOrder.new(^order_b))
+    end
+  end
+end
