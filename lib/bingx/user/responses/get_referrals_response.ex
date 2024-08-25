c0b3f00ea34989649cdc560883cb8c5058900cba
centurion <centurionoff.dev@proton.me>
2024-08-25 13:59:44 -0300

    chore: case for no data when get referrals

--- lib/bingx/user/responses/get_referrals_response.ex
+++ lib/bingx/user/responses/get_referrals_response.ex
@@ -1,9 +1,9 @@
 defmodule BingX.User.GetReferralsResponse do
   alias BingX.User.ReferralInfo
 
-  defstruct [:list, :total]
+  defstruct [list: [], total: 0]
 
-  def new(data) do
+  def new(%{} = data) do
     list = data["list"] || []
 
     %__MODULE__{
@@ -11,4 +11,6 @@ defmodule BingX.User.GetReferralsResponse do
       total: length(list)
     }
   end
+
+  def new(_data), do: %__MODULE__{}
 end
